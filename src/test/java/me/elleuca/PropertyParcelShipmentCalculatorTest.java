ae9a1d2b2a6077bfb6f097b9b3b485164008ec92
Luca Ferretti <elle.uca@gmail.com>
2019-04-16 23:12:55 +0200

    property testing with quickcheck

---
+++ src/test/java/me/elleuca/PropertyParcelShipmentCalculatorTest.java
@@ -0,0 +1,29 @@
+package me.elleuca;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitQuickcheck.class)
+public class PropertyParcelShipmentCalculatorTest {
+
+    @Property(trials = 400)
+    public void ShipmentNotFreeForParcelPriceBelow20Euros(@InRange(minDouble = 0.0, maxDouble = 19.9999) double price) {
+        PostalService postalService = new PostalService();
+        boolean isFreeShipment = postalService.isFreeShipment(price);
+
+        assertThat(isFreeShipment, is(false));
+    }
+
+    @Property(trials = 400)
+    public void ShipmentFreeForParcelPriceAbove20Euros(@InRange(minDouble = 20.0, maxDouble = 1.0E10) double price) {
+        PostalService postalService = new PostalService();
+        boolean isFreeShipment = postalService.isFreeShipment(price);
+
+        assertThat(isFreeShipment, is(true));
+    }
+}
